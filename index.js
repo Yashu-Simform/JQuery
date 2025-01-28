b8f06000ca9ebc1cccc2806b5407f98d6edb5ff7
Yashu Ranparia <yashu.ranparia@simformsolutions.com>
2025-01-28 13:37:14 +0530

    Task Completed

--- index.js
+++ index.js
@@ -17,120 +17,231 @@ $(document).ready(
                     const requiredCols = ['firstname', 'lastname', 'email', 'birthDate', 'phone', 'website', 'company']
                     const colNames = ['FirstName' , 'LastName', 'Email', 'BirthDate', 'Phone', 'Website', 'Company']
 
+                    $('#searchbar').val('')
                 
                     
-                    let columnHeads = $('<tr></tr>')
-                    const keys = Object.keys(responseData[0]);
+                    function setColHeads(responseData, colNames){
+                        let columnHeads = $('<tr></tr>')
+                        const keys = Object.keys(responseData[0]);
 
-                    // Setting the heading of the columns
-                    for(let colh of colNames){
-                        let tdel = $('<td></td>').text(colh).height(30).width(60).css(
-                            {
-                                'border': '2px solid black', 
-                                'border-collapse': 'collapse', 
-                                'text-align': 'center',
-                                'margin': '0',
-                                'padding': '4px 8px',
-                                'font-weight': '600'
-                            }
-                        )
-                        console.log(keys[colh])
+                        // Setting the heading of the columns
+                        for(let colh of colNames){
+                            let tdel = $('<td></td>').text(colh).height(30).width(60).css(
+                                {
+                                    'border': '2px solid black', 
+                                    'border-collapse': 'collapse', 
+                                    'text-align': 'center',
+                                    'margin': '0',
+                                    'padding': '4px 8px',
+                                    'font-weight': '600'
+                                }
+                            )
 
-                        columnHeads.append(tdel)
+                            columnHeads.append(tdel)
+                        }
+                        columnHeads.attr('id', 'colHeads')
+                        $('#mytable').append(columnHeads)
                     }
-                    columnHeads.attr('id', 'colHeads')
-                    $('#mytable').append(columnHeads)
 
 
                     // Adding each records as rows
-                    for(let r = 0;r<totalRows;r++){
-                        let row = $('<tr></tr>')
-                        for(let colh = 0; colh<requiredCols.length;colh++){
-                            let dataVal = (requiredCols[colh] == 'company') ? responseData[r][requiredCols[colh]]['name'] : responseData[r][requiredCols[colh]]
-                            console.log(dataVal)
-                            let tdel = $('<td></td>').text(dataVal).height(30).width(60).css( 
-                                {
-                                    'border-collapse': 'collapse',
-                                    'padding': '4px 8px',
-                                    'border': '2px solid grey', 
-                                    'text-align': 'center',
-                                }
-                            )
-                            row.append(tdel)
+                    function addRecords(responseData, totalRows, requiredCols){
+                        for(let r = 0;r<totalRows;r++){
+                            let row = $('<tr></tr>').attr('id', responseData[r]['id'])
+                            for(let colh = 0; colh<requiredCols.length;colh++){
+                                let dataVal = (requiredCols[colh] == 'company') ? responseData[r][requiredCols[colh]]['name'] : responseData[r][requiredCols[colh]]
+                                
+                                let tdel = $('<td></td>').text(dataVal).height(30).width(60).css( 
+                                    {
+                                        'border-collapse': 'collapse',
+                                        'padding': '4px 8px',
+                                        'border': '2px solid grey', 
+                                        'text-align': 'center',
+                                    }
+                                )
+                                row.append(tdel)
+                            }
+                            $('#mytable').append(row)
                         }
-                        $('#mytable').append(row)
                     }
 
 
                     //Making the rows clickable
-
-
-                    $('tr').not('#colHeads').click(
-                        function(){
-                            let popup = $('<div></div>')
-                            popup.addClass('popup')
-                            $('body').append(popup)
-
-                            let closebtn = $('<span></span>').text('X')
-                                            .css(
-                                                {
-                                                    'position': 'absolute',
-                                                    'right': '2px',
-                                                    'top': '2px',
-                                                    'cursor': 'pointer',
-                                                    'border': '2px solid red',
-                                                    'border-radius': '4px',
-                                                    'padding': '4px'
-                                                }
-                                            )
-                                            .click(
-                                                function(){
-                                                    $(this).parent().hide()
-                                                }
-                                            )
-                            popup.append(closebtn)
-
-                            const data = $(this).children()
-                            let ind = 0
-                            for(d of data){
-                                let p = $('<p></p>')
-                                .append(
-                                    $('<span></span>')
-                                    .text(`${colNames[ind++]}: `)
+                    function rowClicks(colNames){
+                        $('tr').not('#colHeads').click(
+                            function(){
+                                let popup = $('<div></div>')
+                                popup.addClass('popup')
+                                $('body').append(popup)
+    
+                                let closebtn = $('<span></span>').text('X')
+                                                .css(
+                                                    {
+                                                        'position': 'absolute',
+                                                        'right': '2px',
+                                                        'top': '2px',
+                                                        'cursor': 'pointer',
+                                                        'border': '2px solid red',
+                                                        'border-radius': '4px',
+                                                        'padding': '4px'
+                                                    }
+                                                )
+                                                .click(
+                                                    function(){
+                                                        $(this).parent().hide()
+                                                    }
+                                                )
+                                popup.append(closebtn)
+    
+                                const data = $(this).children()
+                                let ind = 0
+                                for(d of data){
+                                    let p = $('<p></p>')
+                                    .append(
+                                        $('<span></span>')
+                                        .text(`${colNames[ind++]}: `)
+                                        .css(
+                                            {
+                                                'font-weight': 500
+                                            }
+                                        )
+                                    )
+                                    .append(
+                                        $('<span></span>')
+                                        .text(d.textContent)
+                                    )
                                     .css(
                                         {
-                                            'font-weight': 500
+                                            'text-align': 'center'
                                         }
                                     )
-                                )
-                                .append(
-                                    $('<span></span>')
-                                    .text(d.textContent)
-                                )
-                                .css(
-                                    {
-                                        'text-align': 'center'
-                                    }
-                                )
-                                popup.append(p)
-                                // console.log(colNames[ind++],': ',d.textContent)
+                                    popup.append(p)
+                                }
+                                popup.toggle()
                             }
-                            popup.toggle()
+                        )
+                    }
+
+                    async function makeTable(responseData, totalRows, requiredCols, colNames){
+                        setColHeads(responseData, colNames);
+                        addRecords(responseData, totalRows, requiredCols);
+                        rowClicks(colNames);
+                    }
+
+                    await makeTable(responseData, totalRows, requiredCols, colNames)
+                    pagination(10, totalRows)
+                    searchBar()
+                }
+
+
+                // Filtering using searchbar
+                function searchBar(){
+                    $('#searchbar').on('input',
+                        function(){
+                            console.log('Search bar is being focused.')
+                            const valu = ($(this).val()).toLowerCase()
+    
+                            const currPage = findCurrPage()
+    
+                            filterLogic(currPage, valu)
                         }
                     )
                 }
 
+                function findCurrPage() {
+                    const currPage = $('#pageNav').children('.active').text()
+                    console.log('Current Page: ', currPage)
 
-                $('#searchbar:focus').focus(
-                    function(){
-                        const valu = $(this).val()
-                        $('tr').not('#colHeads').filter(
+                    return currPage
+                }
+
+                function filterLogic(currPage, valu) {
+                    $(`.page${currPage.toString()}`)
+                    .filter(
+                        function(){
+                            const cs = $(this).children(':first')
+                            const str = (cs[0].textContent).toString().toLowerCase()
+                            if((valu.length == 0) || (str.includes(valu))){
+                                return true;
+                            }else{
+                                return false
+                            }
+                        }
+                    ).show()
+                
+                    $(`.page${currPage.toString()}`).not(
+                        $(`.page${currPage.toString()}`).filter(
                             function(){
-                                // if($(this).children())
+                                const cs = $(this).children(':first')
+                                const str = (cs[0].textContent).toString().toLowerCase()
+                                if((valu.length == 0) || (str.includes(valu))){
+                                    return true;
+                                }else{
+                                    return false
+                                }
                             }
                         )
+                    ).hide()
+                }
+
+
+                // Pagination
+
+                function pagination(thresh, rows){
+                    const pagesNeeded = Math.ceil(rows/thresh)
+                    if(pagesNeeded > 1){
+                        // Apply pagination
+                        const pageNav = $('<div></div>').css(
+                            {
+                                'display': 'flex',
+                                'flex-direction': 'row',
+                                'border-collapse': 'collapse',
+                                'border': '2px solid black',
+                                'background-color': '#fff',
+                                'border-radius': '4px'
+                            }
+                        ).attr('id', 'pageNav')
+
+                        for(let p = 1;p<=pagesNeeded;p++){
+                            for(let el = ((p-1)*thresh);el<((p-1)*thresh)+thresh;el++){
+                                if(el >= rows) break;
+                                ($('tr').not('#colHeads')).filter(
+                                    function(){
+                                        if(($(this).attr('id')).toString() == `${el+1}`){
+                                            return true
+                                        }else{
+                                            return false
+                                        }
+                                    }
+                                ).addClass(`page${p}`)
+                            }
+
+                            pageNav.append(
+                                $('<span></span>')
+                                .text(p.toString())
+                                .click(
+                                    function(){
+                                        $('tr').filter(`.page${p}`).show()
+                                        $('tr').not('#colHeads').not(`.page${p}`).hide()
+                                        $(this).siblings('.active').removeClass('active')
+                                        $(this).addClass('active')
+                                        const currPage = findCurrPage()
+                                        const valu = ($('#searchbar').val()).toLowerCase()
+                                        filterLogic(currPage,valu)
+                                    }
+                                )
+                                .attr('id',`page${p}`)
+                                .addClass('inactive')
+                            )
+                        }
+
+                        $('body').append(pageNav)
                     }
-                )
+
+                    // default
+                    $('tr').not('#colHeads').not('.page1').hide()
+                    $('#page1').addClass('active')
+                }
                 
             }
             
